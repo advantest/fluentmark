fbf8a353b1b0b621df367ddf749ce675c4907b8f
Dietrich Travkin <dietrich.travkin@solunar.de>
2022-09-19 10:38:41 +0200

    HMR-53 Start implementing tests

---
+++ tests/com.advantest.fluentmark.test/src/net/certiv/fluentmark/model/PageRootTest.java
@@ -0,0 +1,27 @@
+/*
+ * This work is made available under the terms of the Eclipse Public License (EPL) Version 1.0.
+ * The EPL 1.0 accompanies this distribution.
+ * 
+ * You may obtain a copy of the License at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ * 
+ * Copyright © 2022-2022 Advantest Europe GmbH. All rights reserved.
+ */
+package net.certiv.fluentmark.model;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class PageRootTest {
+	
+	@Test
+	public void testParsing() {
+		assertTrue(true);
+		assertTrue(false);
+	}
+
+}
