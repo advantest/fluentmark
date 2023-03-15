83485ccd2251f468494bb17cb270ba08dd7e3c0a
Dietrich Travkin <dietrich.travkin@solunar.de>
2023-03-15 14:01:01 +0100

    HMR-78 Refactoring - Extract method

--- net.certiv.fluentmark.ui/src/net/certiv/fluentmark/ui/editor/assist/FileLinkContentAssistProcessor.java
+++ net.certiv.fluentmark.ui/src/net/certiv/fluentmark/ui/editor/assist/FileLinkContentAssistProcessor.java
@@ -51,151 +51,174 @@ public class FileLinkContentAssistProcessor implements IContentAssistProcessor {
 		this.editor = editor;
 	}
 	
+	/**
+	 * We compute completion proposals for links, image references, and link reference definitions.
+	 * 
+	 * 
+	 * @see org.eclipse.jface.text.contentassist.IContentAssistProcessor#computeCompletionProposals(org.eclipse.jface.text.ITextViewer, int)
+	 */
 	@Override
 	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int offset) {
 		IDocument document = viewer.getDocument();
+		
+		IFile currentEditorsMarkdownFile = getCurrentEditorsMarkdownFile();
+		if (currentEditorsMarkdownFile == null) {
+			return NO_PROPOSALS;
+		}
+		
 		try {
 			int currentLine = document.getLineOfOffset(offset);
 			int lineOffset = document.getLineOffset(currentLine);
 			int lineLength = document.getLineLength(currentLine);
-			// TODO check document length
+			
 			String lineLeftFromCursor = lineLength > 0 ? document.get(lineOffset, offset - lineOffset) : "";
 			String lineRightFromCursor = lineLength > 0 ? document.get(offset, lineOffset + lineLength - offset - 1) : "";
 			
-			int indexOfClosingRoundBracket = lineRightFromCursor.indexOf(')');
-			int indexOfOpeningRoundBracket = lineLeftFromCursor.lastIndexOf('(');
-			int indexOfClosingSquareBracket = -1;
-			if (indexOfOpeningRoundBracket > -1
-					&& indexOfOpeningRoundBracket - 1 >= 0
-					&& lineLeftFromCursor.charAt(indexOfOpeningRoundBracket - 1) == ']') {
-				indexOfClosingSquareBracket = indexOfOpeningRoundBracket - 1;
-			}
-			int indexOfOpeningSquareBracket = -1;
-			if (indexOfClosingSquareBracket > -1) {
-				indexOfOpeningSquareBracket = lineLeftFromCursor.substring(0, indexOfClosingSquareBracket).lastIndexOf('[');
-			}
-			
-			// we're not in a link declaration? ==> we have no proposals
-			if (indexOfClosingRoundBracket < 0 || indexOfOpeningRoundBracket < 0
-					|| indexOfClosingSquareBracket < 0 || indexOfOpeningSquareBracket < 0) {
-				return NO_PROPOSALS;
-			}
-			
-			int indexOfExclamationMark = -1;
-			if (indexOfOpeningSquareBracket > -1
-					&& indexOfOpeningSquareBracket - 1 >= 0
-					&& lineLeftFromCursor.charAt(indexOfOpeningSquareBracket - 1) == '!') {
-				indexOfExclamationMark = indexOfOpeningSquareBracket - 1;
-			}
-			
-			String linkTextLeftFromCursor = "";
-			if (indexOfOpeningRoundBracket + 1 < lineLeftFromCursor.length()) {
-				linkTextLeftFromCursor = lineLeftFromCursor.substring(indexOfOpeningRoundBracket + 1); 
-			}
-			
-			String linkTextRightFromCursor = lineRightFromCursor.substring(0, indexOfClosingRoundBracket);
-			
-			// no proposals in case of web addresses
-			if (linkTextLeftFromCursor.startsWith("http://")
-					|| linkTextLeftFromCursor.startsWith("HTTP://")
-					|| linkTextLeftFromCursor.startsWith("https://")
-					|| linkTextLeftFromCursor.startsWith("HTTPS://")) {
-				return NO_PROPOSALS;
-			}
-			
-			IFile currentEditorsMarkdownFile = getCurrentEditorsMarkdownFile();
-			if (currentEditorsMarkdownFile == null) {
-				return NO_PROPOSALS;
-			}
-			
-			IPath currentResourcesAbsolutePath = readCurrentResourcePath(
-					currentEditorsMarkdownFile, linkTextLeftFromCursor);
-			
-			// no proposals if the path is invalid
-			File currentFile = currentResourcesAbsolutePath.toFile();
-			if (!currentFile.exists()) {
-				return NO_PROPOSALS;
-			}
-			
 			ArrayList<ICompletionProposal> proposals = new ArrayList<>();
 			
-			File currentDir =  null;
-			if (currentResourcesAbsolutePath.equals(currentEditorsMarkdownFile.getLocation())) {
-				currentDir = currentFile.getParentFile();
-			} else {
-				if (currentFile.isDirectory()) {
-					currentDir = currentFile;
-					// propose contained files in the next steps
-				} else if (currentFile.isFile()) {
-					// we have already a complete file path, no other proposals
-					return NO_PROPOSALS;
-				}
-			}
-			
-			if (currentDir != null) {
-				proposals.add(new FilePathCompletionProposalWithDialog(editor,
-						offset - linkTextLeftFromCursor.length(), linkTextLeftFromCursor.length() + linkTextRightFromCursor.length()));
-				
-				
-				List<File> files = Arrays.asList(currentDir.listFiles());
-				files.sort(new Comparator<File>() {
-
-					@Override
-					public int compare(File file1, File file2) {
-						boolean file1IsFile = file1.isFile();
-						boolean file2IsFile = file2.isFile();
-						
-						if (file1IsFile && !file2IsFile) {
-							return -1;
-						} else if (file2IsFile && !file1IsFile) {
-							return 1;
-						}
-						
-						return file1.getName().compareTo(file2.getName());
-					}
-					
-				});
-				
-				for (File fileInDir : files) {
-					// skip the file we have already open in editor
-					if (fileInDir.equals(currentEditorsMarkdownFile.getLocation().toFile())) {
-						continue;
-					}
-					
-					String filePathSegment = fileInDir.getName();
-					if (!linkTextLeftFromCursor.isBlank()
-							&& !linkTextLeftFromCursor.endsWith("/")) {
-						filePathSegment = '/' + filePathSegment;
-					}
-					
-					
-					Image img = null;
-					if (fileInDir.isDirectory()) {
-						img = FluentUI.getDefault().getImageProvider().get(FluentImages.DESC_OBJ_FOLDER); 
-					} else {
-						img = FluentUI.getDefault().getImageProvider().get(FluentImages.DESC_OBJ_FILE);
-					}
-					
-					proposals.add(new CompletionProposal(filePathSegment, offset, linkTextRightFromCursor.length(), filePathSegment.length(), img, null, null, null));
-				}
-				
-				String parentDir = currentDir.getParent();
-				if (parentDir != null) {
-					String toParentPathSegment = "..";
-					if (!linkTextLeftFromCursor.isBlank()
-							&& !linkTextLeftFromCursor.endsWith("/")) {
-						toParentPathSegment = '/' + toParentPathSegment;
-					}
-					
-					Image img = FluentUI.getDefault().getImageProvider().get(FluentImages.DESC_OBJ_FOLDER_UP); 
-					proposals.add(new CompletionProposal(toParentPathSegment, offset, linkTextRightFromCursor.length(), toParentPathSegment.length(), img, null, null, null));
-				}
-			}
+			addProposalsForLinksAndImages(proposals, currentEditorsMarkdownFile,
+					offset, currentLine, lineOffset, lineLength, lineLeftFromCursor, lineRightFromCursor);
 			
 			return proposals.toArray(new ICompletionProposal[proposals.size()]);
 		} catch (BadLocationException e) {
 			FluentUI.log(IStatus.ERROR, "Failed reading document for code assist proposals.", e);
 			return NO_PROPOSALS;
+		}
+	}
+	
+	private void addProposalsForLinksAndImages(List<ICompletionProposal> proposals, IFile currentEditorsMarkdownFile,
+			int offset, int currentLine, int lineOffset, int lineLength, String lineLeftFromCursor, String lineRightFromCursor) {
+		/*
+		 *  Try to detect if we're in a statement like
+		 *  [title](file reference path)
+		 *  or
+		 *  ![title](file reference path)
+		 *  where the cursor is between the "(" and ")" brackets.
+		 */
+		int indexOfClosingRoundBracket = lineRightFromCursor.indexOf(')');
+		int indexOfOpeningRoundBracket = lineLeftFromCursor.lastIndexOf('(');
+		int indexOfClosingSquareBracket = -1;
+		if (indexOfOpeningRoundBracket > -1
+				&& indexOfOpeningRoundBracket - 1 >= 0
+				&& lineLeftFromCursor.charAt(indexOfOpeningRoundBracket - 1) == ']') {
+			indexOfClosingSquareBracket = indexOfOpeningRoundBracket - 1;
+		}
+		int indexOfOpeningSquareBracket = -1;
+		if (indexOfClosingSquareBracket > -1) {
+			indexOfOpeningSquareBracket = lineLeftFromCursor.substring(0, indexOfClosingSquareBracket).lastIndexOf('[');
+		}
+		
+		// we're not in a link declaration? ==> we have no proposals
+		if (indexOfClosingRoundBracket < 0 || indexOfOpeningRoundBracket < 0
+				|| indexOfClosingSquareBracket < 0 || indexOfOpeningSquareBracket < 0) {
+			return;
+		}
+		
+		// TODO only propose image files in case we're in an image reference?
+		int indexOfExclamationMark = -1;
+		if (indexOfOpeningSquareBracket > -1
+				&& indexOfOpeningSquareBracket - 1 >= 0
+				&& lineLeftFromCursor.charAt(indexOfOpeningSquareBracket - 1) == '!') {
+			indexOfExclamationMark = indexOfOpeningSquareBracket - 1;
+		}
+		
+		String linkTextLeftFromCursor = "";
+		if (indexOfOpeningRoundBracket + 1 < lineLeftFromCursor.length()) {
+			linkTextLeftFromCursor = lineLeftFromCursor.substring(indexOfOpeningRoundBracket + 1); 
+		}
+		
+		String linkTextRightFromCursor = lineRightFromCursor.substring(0, indexOfClosingRoundBracket);
+		
+		// no proposals in case of web addresses
+		if (linkTextLeftFromCursor.startsWith("http://")
+				|| linkTextLeftFromCursor.startsWith("HTTP://")
+				|| linkTextLeftFromCursor.startsWith("https://")
+				|| linkTextLeftFromCursor.startsWith("HTTPS://")) {
+			return;
+		}
+		
+		IPath currentResourcesAbsolutePath = readCurrentResourcePath(
+				currentEditorsMarkdownFile, linkTextLeftFromCursor);
+		
+		// no proposals if the path is invalid
+		File currentFile = currentResourcesAbsolutePath.toFile();
+		if (!currentFile.exists()) {
+			return;
+		}
+		
+		File currentDir =  null;
+		if (currentResourcesAbsolutePath.equals(currentEditorsMarkdownFile.getLocation())) {
+			currentDir = currentFile.getParentFile();
+		} else {
+			if (currentFile.isDirectory()) {
+				currentDir = currentFile;
+				// propose contained files in the next steps
+			} else if (currentFile.isFile()) {
+				// we have already a complete file path, no other proposals
+				return;
+			}
+		}
+		
+		if (currentDir == null) {
+			return;
+		}
+		
+		proposals.add(new FilePathCompletionProposalWithDialog(editor,
+				offset - linkTextLeftFromCursor.length(), linkTextLeftFromCursor.length() + linkTextRightFromCursor.length()));
+		
+		
+		List<File> files = Arrays.asList(currentDir.listFiles());
+		files.sort(new Comparator<File>() {
+
+			@Override
+			public int compare(File file1, File file2) {
+				boolean file1IsFile = file1.isFile();
+				boolean file2IsFile = file2.isFile();
+				
+				if (file1IsFile && !file2IsFile) {
+					return -1;
+				} else if (file2IsFile && !file1IsFile) {
+					return 1;
+				}
+				
+				return file1.getName().compareTo(file2.getName());
+			}
+			
+		});
+		
+		for (File fileInDir : files) {
+			// skip the file we have already open in editor
+			if (fileInDir.equals(currentEditorsMarkdownFile.getLocation().toFile())) {
+				continue;
+			}
+			
+			String filePathSegment = fileInDir.getName();
+			if (!linkTextLeftFromCursor.isBlank()
+					&& !linkTextLeftFromCursor.endsWith("/")) {
+				filePathSegment = '/' + filePathSegment;
+			}
+			
+			
+			Image img = null;
+			if (fileInDir.isDirectory()) {
+				img = FluentUI.getDefault().getImageProvider().get(FluentImages.DESC_OBJ_FOLDER); 
+			} else {
+				img = FluentUI.getDefault().getImageProvider().get(FluentImages.DESC_OBJ_FILE);
+			}
+			
+			proposals.add(new CompletionProposal(filePathSegment, offset, linkTextRightFromCursor.length(), filePathSegment.length(), img, null, null, null));
+		}
+		
+		String parentDir = currentDir.getParent();
+		if (parentDir != null) {
+			String toParentPathSegment = "..";
+			if (!linkTextLeftFromCursor.isBlank()
+					&& !linkTextLeftFromCursor.endsWith("/")) {
+				toParentPathSegment = '/' + toParentPathSegment;
+			}
+			
+			Image img = FluentUI.getDefault().getImageProvider().get(FluentImages.DESC_OBJ_FOLDER_UP); 
+			proposals.add(new CompletionProposal(toParentPathSegment, offset, linkTextRightFromCursor.length(), toParentPathSegment.length(), img, null, null, null));
 		}
 	}
 	
