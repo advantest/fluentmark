aab7615c04f9641f6d83ebb4b5e05356db6d4a9b
Dietrich Travkin <dietrich.travkin@solunar.de>
2024-02-27 23:26:45 +0100

    HMR-123 Add new test case

--- com.advantest.fluentmark.ui.tests/src/net/certiv/fluentmark/core/convert/ConverterIT.java
+++ com.advantest.fluentmark.ui.tests/src/net/certiv/fluentmark/core/convert/ConverterIT.java
@@ -9,26 +9,36 @@
  */
 package net.certiv.fluentmark.core.convert;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import java.io.File;
-import java.io.IOException;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import net.certiv.fluentmark.core.markdown.MarkdownPartitions;
+import net.certiv.fluentmark.core.util.FluentPartitioningTools;
+import net.certiv.fluentmark.ui.editor.text.MarkdownPartioningTools;
 
 public class ConverterIT {
 	
 	private IConfigurationProvider configProvider;
 	private Converter converter;
 	
-	@TempDir File tempDir;
+	@TempDir
+	File tempDir;
 	
 	@BeforeEach
 	public void setUp() {
@@ -36,23 +46,61 @@ public class ConverterIT {
 		converter = new Converter(configProvider);
 	}
 	
+	private void setupDocumentPartitioner(IDocument document) {
+		FluentPartitioningTools.setupDocumentPartitioner(
+				document,
+				MarkdownPartioningTools.getTools().createDocumentPartitioner(),
+				MarkdownPartitions.FLUENT_MARKDOWN_PARTITIONING);
+	}
+	
 	@Test
 	public void test() throws IOException {
 		// given
-		Path path = Paths.get("resources/parsing/feature-overview.md");
+		Path path = Paths.get("resources/feature-overview.md");
 		String documentContent = Files.readString(path);
 		IDocument document = new Document(documentContent);
 		File tmpFile = new File(tempDir, "feature-overview.md");
 		Files.copy(path, tmpFile.toPath());
+		setupDocumentPartitioner(document);
 		
 		// when
-		String result = converter.convert(new org.eclipse.core.runtime.Path(tmpFile.getAbsolutePath()), tempDir.getAbsolutePath(), document, Kind.VIEW);
+		String result = converter.convert(
+				new org.eclipse.core.runtime.Path(tmpFile.getAbsolutePath()),
+				tempDir.getAbsolutePath(),
+				document,
+				Kind.VIEW);
 		
 		// then
-		Assertions.assertNotNull(result);
-		Assertions.assertFalse(result.isBlank());
+		assertNotNull(result);
+		assertFalse(result.isBlank());
 		
 		// TODO check result contents
+	}
+	
+	@Test
+	public void includeNonExistingPumlFileDoesntThrowException() throws Exception {
+		String markdownFileContent = "# Test\n\n![alt text](../diagrams/none.puml) ";
+		IDocument document = new Document(markdownFileContent);
+		File markdownFile = new File(tempDir, "include_missing_puml_file.md");
+		Path markdownFilePath = Files.createFile(markdownFile.toPath());
+		Files.writeString(markdownFilePath, markdownFileContent, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+		setupDocumentPartitioner(document);
+		
+		String result = null;
+		try {
+			result = converter.convert(
+					new org.eclipse.core.runtime.Path(markdownFile.getAbsolutePath()),
+					tempDir.getAbsolutePath(),
+					document,
+					Kind.VIEW);
+		} catch (Exception e) {
+			fail("Converter is not expected to throw exceptions.", e);
+		}
+		
+		
+		assertNotNull(result);
+		assertFalse(result.isBlank());
+		assertTrue(result.contains("Could not convert Markdown to HTML."));
 	}
 
 }
