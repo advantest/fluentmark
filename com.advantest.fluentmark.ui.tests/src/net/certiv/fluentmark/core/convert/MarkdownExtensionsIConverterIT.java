cf8230412c2eaa9f77d5bd916946d8db2a983a7c
Dietrich Travkin <dietrich.travkin@solunar.de>
2024-05-06 18:21:58 +0200

    HMR-118 Add test cases for header ids in Markdown

---
+++ com.advantest.fluentmark.ui.tests/src/net/certiv/fluentmark/core/convert/MarkdownExtensionsIConverterIT.java
@@ -0,0 +1,55 @@
+package net.certiv.fluentmark.core.convert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+
+import org.eclipse.jface.text.IDocument;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+public class MarkdownExtensionsIConverterIT extends AbstractConverterIT {
+	
+	@ParameterizedTest
+	@EnumSource(names = { "FLEXMARK" })
+	public void noImplicitHeaderIdsRenderedToHtml(ConverterType converterType) throws Exception {
+		configProvider.setConverterType(converterType);
+		
+		String markdownFileContent = "# Heading without ID";
+		IDocument document = prepareDocument(markdownFileContent);
+		File markdownFile = createFileWithContent("header_without_id.md", markdownFileContent);
+		
+		String result = null;
+		try {
+			result = convert(markdownFile, document);
+		} catch (Exception e) {
+			fail("Converter is not expected to throw exceptions.", e);
+		}
+        
+		assertNotNull(result);
+		assertEquals("<h1>Heading without ID</h1>\n", result);
+	}
+	
+	@ParameterizedTest
+	@EnumSource(names = { "PANDOC", "FLEXMARK" })
+	public void explicitHeaderIdsRenderedToHtml(ConverterType converterType) throws Exception {
+		configProvider.setConverterType(converterType);
+		
+		String markdownFileContent = "# Heading with ID {#heading-id-1}";
+		IDocument document = prepareDocument(markdownFileContent);
+		File markdownFile = createFileWithContent("header_with_id.md", markdownFileContent);
+		
+		String result = null;
+		try {
+			result = convert(markdownFile, document);
+		} catch (Exception e) {
+			fail("Converter is not expected to throw exceptions.", e);
+		}
+        
+		assertNotNull(result);
+		assertEquals("<h1 id=\"heading-id-1\">Heading with ID</h1>\n", result);
+	}
+
+}
