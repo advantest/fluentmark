6ecb930b22b22230276f3b4f65bd19de2d73956e
Dietrich Travkin <dietrich.travkin@solunar.de>
2025-06-17 15:43:36 +0200

    HMR-228 Add tests for not checking links in comments and code blocks

--- com.advantest.fluentmark.ui.tests/src/net/certiv/fluentmark/ui/validation/MarkdownLinkValidatorIT.java
+++ com.advantest.fluentmark.ui.tests/src/net/certiv/fluentmark/ui/validation/MarkdownLinkValidatorIT.java
@@ -276,6 +276,86 @@ public class MarkdownLinkValidatorIT {
 	}
 	
 	@Test
+	public void linksInCommentsAreNotChecked() throws Exception {
+		// given
+		String fileContents = """
+				Some text with a [link](https://www.google.com) in it.
+				
+				<!--[This comment contains a link](https://www.ignored.com)-->
+				
+				 <!-- Some more links
+				    [PlantUML](https://plantuml.com)
+				    and [test](path/to/file.puml) -->
+				
+				<!-- ![alt](dir/file.png) -->
+				""";
+		
+		// when
+		document = validateDocument(fileContents);
+		
+		//then
+		verify(linkValidator, times(1)).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[link](https://www.google.com)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[This comment contains a link](https://www.ignored.com)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[PlantUML](https://plantuml.com)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[test](path/to/file.puml)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("![alt](dir/file.png)"), any());
+		
+		verify(linkValidator, times(1)).validateLinkStatement(any(), eq(document), eq(file), any(), any());
+	}
+	
+	@Test
+	public void linksInCodeBlocksAreNotChecked() throws Exception {
+		// given
+		String fileContents = """
+				# Heading
+				
+				```markdown
+				Some Markdown code
+				with links like this [example](path/to/missing-file.md).
+				
+				![](path/to/image.png)
+				
+				[PlantUML](https://plantuml.com)
+				```
+				
+				A link [to ensure](https://www.test.de) some links are found.
+				
+				~~~
+				More code with [Markdown links](path/to/file.txt)
+				
+				![](path/to/image.puml)
+				
+				[Bitbucket](https://www.bitbucket.com)
+				~~~
+				
+				Indented code block:
+				
+				    This is code, too
+				    Link here [are ignored](https://missing.org)
+				    
+				    ![diagram](file.svg)
+				    [](no/file.md)
+				""";
+		
+		// when
+		document = validateDocument(fileContents);
+		
+		//then
+		verify(linkValidator, times(1)).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[to ensure](https://www.test.de)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[example](path/to/missing-file.md)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("![](path/to/image.png)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[PlantUML](https://plantuml.com)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[Markdown links](path/to/file.txt)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("![](path/to/image.puml)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[Bitbucket](https://www.bitbucket.com)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[are ignored](https://missing.org)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("![diagram](file.svg)"), any());
+		verify(linkValidator, never()).validateLinkStatement(any(), eq(document), eq(file), matchedTextEq("[](no/file.md)"), any());
+		
+		verify(linkValidator, times(1)).validateLinkStatement(any(), eq(document), eq(file), any(), any());
+	}
+	
+	@Test
 	public void testFileAndFolderPathValidation() throws Exception {
 		// given
 		String fileContents = """
