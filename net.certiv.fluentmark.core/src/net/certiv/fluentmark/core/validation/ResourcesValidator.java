2fd7f69b463351c9387930fa3df036b952618a6d
Dietrich Travkin <dietrich.travkin@solunar.de>
2025-07-17 16:33:43 +0200

    HMR-240 Start implementing a new validator
    It will replace parts of the MarkerCalculator from ui plug-in.

---
+++ net.certiv.fluentmark.core/src/net/certiv/fluentmark/core/validation/ResourcesValidator.java
@@ -0,0 +1,141 @@
+package net.certiv.fluentmark.core.validation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITypedRegion;
+
+import net.certiv.fluentmark.core.FluentCore;
+import net.certiv.fluentmark.core.partitions.IDocumentPartitioner;
+import net.certiv.fluentmark.core.util.FileUtils;
+
+
+public class ResourcesValidator {
+	
+	// TODO collect all ITypedRegionValidators
+	private List<ITypedRegionValidator> validators;
+	
+	// TODO somewhere set the IValidationResultConsumer
+	private IValidationResultConsumer validationResultConsumer;
+	
+	public void performResourceValidation(IDocument document, IFile file, IProgressMonitor monitor) {
+		if (file == null || monitor == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		if (monitor.isCanceled()) {
+			return;
+		}
+		
+		// The file could have been deleted / moved after scheduling the marker calculation
+		if (!file.exists()) {
+			return;
+		}
+		
+		List<ITypedRegionValidator> responsibleValidators = validators.stream()
+			.filter(validator -> validator.isValidatorFor(file))
+			.toList();
+		
+		if (responsibleValidators.isEmpty()) {
+			return;
+		}
+		
+		if (document == null) {
+			// Sometimes file.exists() returns true, although the file does no longer exist => We refresh the file system state to avoid that.
+			refreshFileStatus(file, monitor);
+			
+			if (!file.exists()) {
+				return;
+			}
+			
+			document = readFile(file);
+			
+			if (document == null) {
+				return;
+			}
+		}
+		
+		Map<String,List<ITypedRegionValidator>> partitioningToValidatorsMap = new HashMap<>();
+		for (ITypedRegionValidator validator: responsibleValidators) {
+			String partitioning = validator.getRequiredPartitioning(file);
+			
+			if (partitioning == null || partitioning.isBlank()) {
+				FluentCore.log(IStatus.ERROR, String.format("Validator %s requires illegal (empty) partitioning", validator.getClass().getName()));
+				continue;
+			}
+			
+			List<ITypedRegionValidator> validatorsForPartioning = partitioningToValidatorsMap.get(partitioning);
+			if (validatorsForPartioning == null) {
+				validatorsForPartioning = new ArrayList<>();
+				partitioningToValidatorsMap.put(partitioning, validatorsForPartioning);
+			}
+			validatorsForPartioning.add(validator);
+		}
+		
+		for (String partitioning: partitioningToValidatorsMap.keySet()) {
+			if (monitor.isCanceled()) {
+				return;
+			}
+			
+			ITypedRegion[] partitions = computePartitions(document, file, partitioning);
+			
+			if (partitions == null || (partitions.length == 0 && document.getLength() != 0)) {
+				FluentCore.log(IStatus.WARNING, String.format("Could not calculate partitions for file %s.", file.getLocation().toString()));
+				return;
+			}
+			
+			for (ITypedRegionValidator validator : partitioningToValidatorsMap.get(partitioning)) {
+				for (ITypedRegion region: partitions) {
+					if (monitor.isCanceled()) {
+						return;
+					}
+					
+					if (validator.isValidatorFor(region, file) ) {
+						validator.validateRegion(region, document, file, validationResultConsumer);
+					}
+				}
+			}
+		}
+	}
+	
+	private void refreshFileStatus(IFile file, IProgressMonitor monitor) {
+		if (file != null) {
+			try {
+				file.refreshLocal(IResource.DEPTH_ZERO, monitor);
+			} catch (CoreException e) {
+				FluentCore.log(IStatus.ERROR, "Could not refresh file status for file " + file.getLocation().toString(), e);
+			}
+		}
+	}
+	
+	private IDocument readFile(IFile file) {
+		try {
+			String fileContents = FileUtils.readFileContents(file);
+			return new Document(fileContents);
+		} catch (Exception e) {
+			FluentCore.log(IStatus.ERROR, "Failed reading / valdating file " + file.getLocation().toString(), e);
+		}
+		
+		return null;
+	}
+	
+	private ITypedRegion[] computePartitions(IDocument document, IFile file, String partitioning) {
+		// TODO find IDocumentPartitioner for given partitioning
+		IDocumentPartitioner partitioner = null;
+		
+		// TODO can we check if the partitioner we need is already set up?
+		partitioner.setupDocumentPartitioner(document, file);
+		
+		return partitioner.computePartitioning(document, file);
+	}
+
+}
